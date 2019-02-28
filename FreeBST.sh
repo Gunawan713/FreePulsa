82cde1d4b47a7b721557c748481163edd66aba0c
Mr.N4w4Nu6 <47638506+Gunawan713@users.noreply.github.com>
2019-03-01 06:13:53 +0700

    Add files via upload

---
+++ FreeBST.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Lumayan Buat Pamer KeKwan Lu😂
+#Tools Ini Cuman Buat Iseng² doang
+#jangan recode yh
+#recode tools orang tdk menjadikan kamu mastah
+
+echo "\033[0;31m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
+===========UU====UU==============
++          *//||\\*             +
++            ****               +
++ coded by : Mr.N4w4Nu6         +
++ Team : BL4CKSH4D0WTE4M    +
++ kontak me : 6283145204326     +
++                               +
++  ^...^                 ^...^  +
++ <_* *_> TransferPulsa <_* *_> +
++   \_/                   \_/   +
+================================="
+
+echo "\033[0;32m]Masukan Nomer Anda: "
+read anda
+echo "\033[0;32m]Masukan Nomer Korban: "
+read korban
+echo "\033[0;32m]Jumlah Pulsa : "
+read pulsa
+echo
+echo "Wait"
+echo
+echo "$korban, Sukses Ngirim Pulsa $pulsa"
+echo "Done BosQ"
